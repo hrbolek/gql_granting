9668fc5526110f20979a36d6f5e9bbd938ada951
Ondrej Klement <ondrej.klement@unob.cz>
2023-12-31 23:03:30 +0100

    Small clean-up

--- gql_granting/utils/DBFeeder.py
+++ gql_granting/utils/DBFeeder.py
@@ -43,12 +43,13 @@ from gql_granting.DBDefinitions import (
     ClassificationModel,
     ClassificationTypeModel,
 
-
+    ProgramStudents,
 )
 
 import asyncio
 import os
 import json
+import uuid
 
 from uoishelpers.feeders import ImportModels
 
@@ -67,18 +68,29 @@ def get_demodata():
                         dateValueWOtzinfo = None
                 
                 json_dict[key] = dateValueWOtzinfo
+            
+            if (key in ["id", "changedby", "createdby"]) or (key.endswith("_id")):
+                
+                if key == "outer_id":
+                    json_dict[key] = value
+                elif value not in ["", None]:
+                    json_dict[key] = uuid.UUID(value)
+                else:
+                    pass
+                    #print(key, value)
+            #if (key == "event_id"): print(key, value)
+
         return json_dict
-
-
-    with open("./systemdata.json", "r") as f:
+    
+    with open("./systemdata.json", "r", encoding="utf-8") as f:
         jsonData = json.load(f, object_hook=datetime_parser)
 
-    return jsonData
+    return jsonData    
 
 async def initDB(asyncSessionMaker):
 
-    defaultNoDemo = "_________"
-    if defaultNoDemo == os.environ.get("DEMO", defaultNoDemo):
+    demo = os.environ.get("DEMO", None)
+    if demo not in [None, "true"]:
         dbModels = [
             ProgramFormTypeModel,
             ProgramLanguageTypeModel,
@@ -86,15 +98,9 @@ async def initDB(asyncSessionMaker):
             ProgramTitleTypeModel,
             ProgramTypeModel,
             LessonTypeModel,
+
             ClassificationLevelModel,
             ClassificationTypeModel,
-
-            ProgramModel,
-            SubjectModel,
-            SemesterModel,
-            TopicModel,
-            LessonModel,
-            ClassificationModel,
         ]
     else:
         dbModels = [
@@ -113,7 +119,7 @@ async def initDB(asyncSessionMaker):
             TopicModel,
             LessonModel,
             ClassificationModel,
-
+            ProgramStudents
         ]
         
     jsonData = get_demodata()
