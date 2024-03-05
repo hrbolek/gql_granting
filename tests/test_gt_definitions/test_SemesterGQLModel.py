aaab438b39bf43ca0e84e1767dc4e8f5c5e70fe3
HungrySaturn <seemann123@post.cz>
2024-03-05 16:01:03 +0100

    fixed testy

--- tests/test_gt_definitions/test_SemesterGQLModel.py
+++ tests/test_gt_definitions/test_SemesterGQLModel.py
@@ -16,21 +16,34 @@ from .gt_utils import (
 )
 
 test_reference_semesters = createResolveReferenceTest(tableName='acsemesters', gqltype='AcSemesterGQLModel', attributeNames=["id"])
-#test_query_semester_page = createPageTest(tableName="acsemesters", queryEndpoint="acsemesterPage")
+test_query_semester_page = createPageTest(tableName="acsemesters", queryEndpoint="acsemesterPage", attributeNames=["id"])
+aaa = createByIdTest(tableName="acsemesters", queryEndpoint="acsemesterById", attributeNames=["id"])
 
 test_semester_insert = createFrontendQuery(query="""
-    mutation($classificationtypeId: UUID!, $subjectId: UUID!) { 
-        result: semesterInsert(semester: {classificationtypeId: $classificationtypeId, subjectId: $subjectId}) { 
+    mutation MyMutation($classificationtypeId: UUID!, $subjectId: UUID!) {
+        semesterInsert(semester: {classificationtypeId: $classificationtypeId, subjectId: $subjectId}) {
             id
             msg
             semester {
-                classificationType { id }
-                subject { id }
+            credits
+            id
+            lastchange
+            order
+            topics {
+                id
+            }
+            classifications {
+                id
+            }
+            classificationType {
+                id
+            }
             }
         }
-    }
+        }
     """, 
-    variables={"classificationtypeId": "a00a0642-b095-11ed-9bd8-0242ac110002", "subjectId": "3ed497ea-ab40-4e94-a93e-9d9627497652"},
+    variables={"classificationtypeId": "a00a0642-b095-11ed-9bd8-0242ac110002",
+  "subjectId": "3ed497ea-ab40-4e94-a93e-9d9627497652"},
     asserts=[]
 )
 
@@ -50,3 +63,4 @@ test_semester_update = createUpdateQuery(
     variables={"id": "85c0db4a-148c-477e-ad5b-3985eaba1f90", "lastchange": datetime.datetime.now().isoformat()},
     tableName="acsemesters"
 )
+
