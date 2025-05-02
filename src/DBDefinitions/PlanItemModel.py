de07e3abb567092b8850dac3d071ede3632f56e1
Hrbolek <alexandr.stefek@unob.cz>
2025-05-02 09:29:02 +0200

    bad foreign keys

--- src/DBDefinitions/PlanItemModel.py
+++ src/DBDefinitions/PlanItemModel.py
@@ -12,8 +12,8 @@ class PlanItemModel(BaseModel):
     length: Mapped[int] = mapped_column(default=None, nullable=True, comment="amount of virtual units")
     order: Mapped[int] = mapped_column(default=None, nullable=True, comment="order in plan")
 
-    plan_id: Mapped[IDType] = mapped_column(ForeignKey("aclessontypes.id"), index=True, default=None, nullable=True)
-    lessontype_id: Mapped[IDType] = mapped_column(ForeignKey("plans.id"), index=True, default=None, nullable=True)
+    plan_id: Mapped[IDType] = mapped_column(ForeignKey("plans.id"), index=True, default=None, nullable=True)
+    lessontype_id: Mapped[IDType] = mapped_column(ForeignKey("aclessontypes.id"), index=True, default=None, nullable=True)
     topic_id: Mapped[IDType] = mapped_column(ForeignKey("actopics.id"), index=True, default=None, nullable=True)
     linked_with_id: Mapped[IDType] = mapped_column(ForeignKey("plan_lessons.id"), index=True, default=None, nullable=True)
     event_id: Mapped[IDType] = UUIDFKey(ForeignKey("events.id"), index=True, default=None, nullable=True)
