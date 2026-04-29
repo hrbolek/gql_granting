c9899af1fff6e010333d0ca509fab6ae2d019140
Hrbolek <profesor@hrbolek.cz>
2026-04-29 19:54:06 +0200

    description  in subject

--- src/DBDefinitions/SubjectModel.py
+++ src/DBDefinitions/SubjectModel.py
@@ -16,6 +16,10 @@ class SubjectModel(BaseModel):
 
     name: Mapped[str] = mapped_column(default=None, nullable=True)
     name_en: Mapped[str] = mapped_column(default=None, nullable=True)
+    
+    description : Mapped[str] = mapped_column(default=None, nullable=True)
+    description_en: Mapped[str] = mapped_column(default=None, nullable=True)
+
     program_id: Mapped[IDType] = mapped_column(ForeignKey("acprograms.id"), index=True, default=None, nullable=True)
     guarantors_group_id: Mapped[IDType] = UUIDFKey(nullable=True)
 
